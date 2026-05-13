9e4d99a836b7b27457f8e12cb5d1d5b9f06dbfd1
otecGlinomes83 <mishakhodyrev0@gmail.com>
2026-05-13 20:37:36 +0500

    работа над бутстрапом так называемым
    я ебал рот этого опуса, я не знаю куда он меня ведет. все мутно. пытаюсь разобраться. Сейчас будет крупное переписывание

---
+++ Assets/Modules/Game/Runtime/GameBootstrap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MadSlime.Movement;
+using MadSlime.Player;
+using UnityEngine;
+
+namespace MadSlime.Game
+{
+    public sealed class GameBootstrap : MonoBehaviour
+    {
+        [SerializeField] private Slime _slimePrefab;
+
+        private void Awake()
+        {
+            Slime slime = Instantiate(_slimePrefab);
+            IMover mover = GetComponent<IMover>();
+            IPlayerInput playerInput = GetComponent<IPlayerInput>();
+            slime.Setup(mover, playerInput);
+        }
+    }
+}
---
+++ Assets/Modules/Player/Runtime/Slime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MadSlime.Movement;
+using UnityEngine;
+
+namespace MadSlime.Player
+{
+    public sealed class Slime : MonoBehaviour
+    {
+        private IMover _mover;
+        private IPlayerInput _playerInput;
+        
+        private bool _isSetupFinished;
+
+        private void Update()
+        {
+            if (_isSetupFinished==false) 
+                return;
+
+            _mover.Move(_playerInput.Direction);
+        }
+
+        public void Setup(IMover mover, IPlayerInput playerInput)
+        {
+            _mover = mover;
+            _playerInput = playerInput;
+            _isSetupFinished = true;
+        }
+    }
+}
