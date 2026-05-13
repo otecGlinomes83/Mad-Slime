1615fdae611a76cc83cabc83260f1777e415795a
otecGlinomes83 <mishakhodyrev0@gmail.com>
2026-05-14 01:12:06 +0500

    начинаем с нуля нахуй

--- Assets/Modules/Movement/Public/IMover.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MadSlime.Movement
-{
-    public interface IMover
-    {
-        public void Move(Vector2 direction);
-    }
-}
--- Assets/Modules/Player/Public/IPlayerInput.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MadSlime.Player
-{
-    public interface IPlayerInput
-    {
-        public Vector2 Direction { get; }
-    }
-}
