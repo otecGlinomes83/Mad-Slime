7fe68fd76c940f24d1e5581ba96bbe49b31a38e1
otecGlinomes83 <mishakhodyrev0@gmail.com>
2026-05-30 10:33:08 +0500

    небольшое дополнение к аттрактору

--- Assets/Scripts/Collector/CollectableAttractor.cs
+++ Assets/Scripts/Collector/CollectableAttractor.cs
@@ -7,6 +7,7 @@ public sealed class CollectableAttractor : MonoBehaviour
     [SerializeField] private float _attractionRadius = 5f;
     [SerializeField] private float _attractionForce = 12f;
     [SerializeField] private LayerMask _attractableMask;
+    [SerializeField] private Player _player;
 
     private readonly Collider[] _attractionBuffer = new Collider[BufferSize];
 
@@ -14,7 +15,8 @@ public sealed class CollectableAttractor : MonoBehaviour
 
     private void Update()
     {
-        int hitsCount = Physics.OverlapSphereNonAlloc(transform.position, _attractionRadius, _attractionBuffer, _attractableMask);
+        int hitsCount =
+            Physics.OverlapSphereNonAlloc(transform.position, _attractionRadius, _attractionBuffer, _attractableMask);
 
         for (int i = 0; i < hitsCount; i++)
         {
@@ -25,7 +27,11 @@ public sealed class CollectableAttractor : MonoBehaviour
                 continue;
             }
 
-            
+            if (attractable.Mass > _player.Mass)
+            {
+                return;
+            }
+
             attractable.Attract(transform.position, _attractionForce);
         }
     }
